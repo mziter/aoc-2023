138533524515daac22aed0328d1ee7d35b53c51e
Matthew Ziter <mziter@gmail.com>
2023-12-04 18:41:38 -0500

    day 5 template

---
+++ day05/src/day05.rs
@@ -0,0 +1,26 @@
+use rayon::prelude::*;
+
+pub fn solve_part_one(input: &str) -> u32 {
+    todo!()
+}
+
+pub fn solve_part_two(input: &str) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = r#""#;
+
+    #[test]
+    fn test_solve_part_one_example() {
+        assert_eq!(solve_part_one(TEST_INPUT), 13);
+    }
+
+    #[test]
+    fn test_solve_part_two() {
+        assert_eq!(solve_part_two(TEST_INPUT), 30);
+    }
+}
