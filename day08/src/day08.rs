c2f9168386bcc5e5250056784ae91e9b54a82d1e
Matt Ziter <mziter@gmail.com>
2023-12-07 14:28:08 -0500

    day 8 template

---
+++ day08/src/day08.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+
+pub fn solve_part_one(input: &str) -> u64 {
+    todo!()
+}
+
+pub fn solve_part_two(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = r#""#;
+
+    #[test]
+    fn test_solve_part_one() {
+        assert_eq!(solve_part_one(TEST_INPUT), 6440);
+    }
+
+    #[test]
+    fn test_solve_part_two() {
+        assert_eq!(solve_part_two(TEST_INPUT), 5905);
+    }
+}
