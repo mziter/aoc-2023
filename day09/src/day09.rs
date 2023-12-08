3ba8ae5895d1139053c6e553443a9be632f447e1
Matthew Ziter <mziter@gmail.com>
2023-12-08 16:24:11 -0500

    add day 9 template

---
+++ day09/src/day09.rs
@@ -0,0 +1,24 @@
+pub fn solve_part_one(input: &str) -> u64 {
+    todo!();
+}
+
+pub fn solve_part_two(input: &str) -> u64 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_EXAMPLE: &str = r#""#;
+
+    #[test]
+    fn test_solve_part_one() {
+        assert_eq!(solve_part_one(TEST_EXAMPLE), 2);
+    }
+
+    #[test]
+    fn test_solve_part_two() {
+        assert_eq!(solve_part_two(TEST_EXAMPLE), 6);
+    }
+}
