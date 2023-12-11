4837d66b31713bf1652d96b2f625175f005119ad
Matt Ziter <mziter@gmail.com>
2023-12-11 08:35:07 -0500

    add day 11

---
+++ day11/src/bin/day11.rs
@@ -0,0 +1,9 @@
+use aoc_2023::day11::*;
+
+fn main() {
+    let p1_answer = solve_part_one(include_str!("../../input/day11.in"));
+    println!("p1: {}", p1_answer);
+
+    let p2_answer = solve_part_two(include_str!("../../input/day11.in"));
+    println!("p2: {}", p2_answer);
+}
---
+++ day11/src/day11.rs
@@ -0,0 +1,23 @@
+pub fn solve_part_one(input: &str) -> u32 {
+    todo!();
+}
+
+pub fn solve_part_two(input: &str) -> u32 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_EXAMPLE: &str = r#""#;
+    #[test]
+    fn test_solve_part_one() {
+        assert_eq!(solve_part_one(TEST_EXAMPLE), 4);
+    }
+
+    #[test]
+    fn test_solve_part_two() {
+        assert_eq!(solve_part_two(TEST_EXAMPLE), 4);
+    }
+}
