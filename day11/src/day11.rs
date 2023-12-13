785676e1b96f7e3957fcdc33e6f3c5a57b66f9f0
Matthew Ziter <mziter@gmail.com>
2023-12-12 22:45:50 -0500

    day 11 part 1

--- day11/src/day11.rs
+++ day11/src/day11.rs
@@ -1,5 +1,89 @@
-pub fn solve_part_one(input: &str) -> u32 {
-    todo!();
+struct TupleCombinationIter<'a, T> {
+    i: usize,
+    j: usize,
+    items: &'a [T],
+}
+
+impl<'a, T> TupleCombinationIter<'a, T> {
+    fn new(items: &[T]) -> TupleCombinationIter<T> {
+        TupleCombinationIter { i: 0, j: 1, items }
+    }
+}
+
+impl<'a, T> Iterator for TupleCombinationIter<'a, T> {
+    type Item = (&'a T, &'a T);
+    fn next(&mut self) -> Option<Self::Item> {
+        // reset if at end of items
+        if self.j == self.items.len() {
+            self.i += 1;
+            if self.i >= self.items.len() {
+                return None;
+            }
+            self.j = self.i + 1;
+            if self.j >= self.items.len() {
+                return None;
+            }
+        }
+
+        let next_item = (&self.items[self.i], &self.items[self.j]);
+
+        // increment
+        self.j += 1;
+
+        Some(next_item)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn dist(&self, other: &Self) -> usize {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+}
+
+pub fn solve_part_one(input: &str) -> usize {
+    let mut cols = vec![1; 140];
+    let mut rows = vec![1; 140];
+    let mut galaxies = vec![];
+
+    input.lines().enumerate().for_each(|(r, row)| {
+        row.chars().enumerate().for_each(|(c, ch)| {
+            if ch == '#' {
+                cols[c] = 0;
+                rows[r] = 0;
+                galaxies.push(Point { x: c, y: r });
+            }
+        })
+    });
+
+    //galaxies
+    //.iter()
+    // this is expensive since combinations will clone and create a
+    // new vector for every pair. We should create our own iterator
+    // that creates tuples with references
+    //.combinations(2)
+    //.map(|galaxy_pair| {
+    TupleCombinationIter::new(&galaxies)
+        .map(|(a, b)| {
+            let additional_y: usize = if a.y > b.y {
+                rows[b.y..=a.y].iter().sum()
+            } else {
+                rows[a.y..=b.y].iter().sum()
+            };
+
+            let additional_x: usize = if a.x > b.x {
+                cols[b.x..=a.x].iter().sum()
+            } else {
+                cols[a.x..=b.x].iter().sum()
+            };
+            a.dist(b) + additional_y + additional_x
+        })
+        .sum()
 }
 
 pub fn solve_part_two(input: &str) -> u32 {
@@ -10,10 +94,19 @@ pub fn solve_part_two(input: &str) -> u32 {
 mod tests {
     use super::*;
 
-    const TEST_EXAMPLE: &str = r#""#;
+    const TEST_EXAMPLE: &str = r#"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."#;
     #[test]
     fn test_solve_part_one() {
-        assert_eq!(solve_part_one(TEST_EXAMPLE), 4);
+        assert_eq!(solve_part_one(TEST_EXAMPLE), 374);
     }
 
     #[test]
